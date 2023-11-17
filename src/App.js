7c2773db2d61a98c5d5cf66251e6d72007b21c46
Lakshay Khattar <lakshaykhattar0208@gmail.com>
2023-11-17 18:25:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import BasicForm from "./components/BasicForm";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <h3>Basic</h3>
+      </nav>
+      <BasicForm />
+    </div>
+  );
+}
+
+export default App;
